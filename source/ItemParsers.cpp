41df91b59099601003aad18b322cfa5a5fc96937
MustafaCOKER <mustafacoker@yandex.com>
2021-11-28 23:11:44 +0300

    multi line reference parsing

--- source/ItemParsers.cpp
+++ source/ItemParsers.cpp
@@ -8,62 +8,74 @@
 #include "ItemParser.hpp"
 #include "EmailAttr.hpp"
 
-uint32_t PARSERS::InitialParser(const char *, EmailAttr& attr, uint32_t endofline)
+uint32_t PARSERS::InitialParser(const char *, EmailAttr &attr, uint32_t endofline)
 {
-    std::cerr << "Please Set Parser for " << attr.GetKey() << std::endl; 
-    return attr.GetKey().size(); 
+    std::cerr << "Please Set Parser for " << attr.GetKey() << std::endl;
+    return attr.GetKey().size();
 }
 
-uint32_t PARSERS::OneLineAttrParser(const char *content, EmailAttr& attr, uint32_t endofline)
-{
-    const uint32_t keysize = attr.GetKey().size(); 
-
-    attr.SetVal(std::make_pair(content+keysize, endofline-keysize));
-
-    BeginEndOffsets be = attr.GetOffsets();
-    std::string ss(be.first, be.second);
-    // std::cout << "Key : [" << attr.GetKey() << "] Value Str : [" << ss << "]\n";
-
-    return endofline;
-}
-
-uint32_t PARSERS::ReferenceParser(const char *content, EmailAttr& attr, uint32_t endofline)
-{
-    const uint32_t keysize = attr.GetKey().size(); 
-    
-    while(*(content + endofline) == '\t')
-        endofline += Util::GotoEndofLine(content+endofline);
-    
-    attr.SetVal(std::make_pair(content+keysize, endofline-keysize));
-    BeginEndOffsets be = attr.GetOffsets();
-    std::string ss(be.first, be.second);
-    // std::cout << "Key : [" << attr.GetKey() << "] Value Str : [" << ss << "]\n";
-
-    return endofline;
-}
- 
-uint32_t PARSERS::ContentAttrParser(const char *content, EmailAttr& attr, uint32_t endofline)
+uint32_t PARSERS::OneLineAttrParser(const char *content, EmailAttr &attr, uint32_t endofline)
 {
     const uint32_t keysize = attr.GetKey().size();
-    const char *curr = content; 
+
+    attr.SetVal(std::make_pair(content + keysize, endofline - keysize));
+
+    BeginEndOffsets be = attr.GetOffsets();
+    std::string ss(be.first, be.second);
+    // std::cout << "Key : [" << attr.GetKey() << "] Value Str : [" << ss << "]\n";
+
+    return endofline;
+}
+
+uint32_t PARSERS::ReferenceParser(const char *content, EmailAttr &attr, uint32_t endofline)
+{
+    const uint32_t keysize = attr.GetKey().size();
+    uint32_t curr = endofline;
+    bool inParsing = false;
+
+    while (*(content + curr) != '\0')
+    {
+        if (!IS_WSPACE(*(content + curr)))
+            if (!inParsing)
+                if (*(content + curr) == '<')
+                    inParsing = true;
+                else
+                    break;
+
+            else if (*(content + curr) == '>')
+                inParsing = false;
+
+        ++curr;
+    }
+
+    attr.SetVal(std::make_pair(content + keysize, curr - keysize));
+    BeginEndOffsets be = attr.GetOffsets();
+    std::string ss(be.first, be.second);
+    std::cout << "Key : [" << attr.GetKey() << "] Value Str : [" << ss << "]\n";
+
+    return curr;
+}
+
+uint32_t PARSERS::ContentAttrParser(const char *content, EmailAttr &attr, uint32_t endofline)
+{
+    const uint32_t keysize = attr.GetKey().size();
+    const char *curr = content;
     const char boundary_key[] = "boundary=\"";
     char boundary[MAX_BOUNDARY_SIZE] = {0};
 
     curr += keysize;
     ++curr; // for space between content type and next keyword
 
-    uint32_t boundary_index = Util::strnchr(content+keysize+1, ' ', endofline-keysize-1);
-    
+    uint32_t boundary_index = Util::strnchr(content + keysize + 1, ' ', endofline - keysize - 1);
 
     curr += boundary_index;
     ++curr; // skip space before boundary
 
-    if (std::strncmp(curr, boundary_key, std::strlen(boundary_key) ) != 0)
+    if (std::strncmp(curr, boundary_key, std::strlen(boundary_key)) != 0)
     {
-        
     }
 
-    std::string boundary_(curr, endofline - (curr-content));
+    std::string boundary_(curr, endofline - (curr - content));
     std::cerr << "boundary : [" << boundary_ << "]\n";
 
     return endofline;
