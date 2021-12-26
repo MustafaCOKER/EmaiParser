dcd3b0d5966ba9e318d1cd9f6bf49b9d78e9ed24
MustafaCOKER <mustafacoker@yandex.com>
2021-12-26 17:25:59 +0300

    cosmotic updates

--- include/ItemParser.hpp
+++ include/ItemParser.hpp
@@ -4,8 +4,8 @@
 
 namespace PARSERS
 {
-    uint32_t InitialParser(const char *, EmailAttr&, uint32_t);
-    uint32_t OneLineAttrParser(const char *, EmailAttr&, uint32_t);
-    uint32_t ReferenceParser(const char *content, EmailAttr& attr, uint32_t);
-    uint32_t ContentAttrParser(const char *, EmailAttr&, uint32_t);
+    uint32_t InitialParser(const char *, EmailAttr &, uint32_t);
+    uint32_t OneLineAttrParser(const char *, EmailAttr &, uint32_t);
+    uint32_t ReferenceParser(const char *content, EmailAttr &attr, uint32_t);
+    uint32_t ContentAttrParser(const char *, EmailAttr &, uint32_t);
 };
